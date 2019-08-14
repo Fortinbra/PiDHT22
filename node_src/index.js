f6bdefe4389dc417d6ad5e8d8d66ef0ae25fc946
Darren Swan <mrdarren@turnsys.com>
2019-08-14 00:22:57 -0500

    Adding Interval

--- node_src/index.js
+++ node_src/index.js
@@ -1,9 +1,12 @@
 var sensor = require("node-dht-sensor");
 
-sensor.read(22, 4, function (err, temperature, humidity) {
-    if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + 'C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
-    }
-});
+function doSomething(arg) {
+    sensor.read(22, 4, function (err, temperature, humidity) {
+        if (!err) {
+            console.log('temp: ' + temperature.toFixed(1) + 'C, ' +
+                'humidity: ' + humidity.toFixed(1) + '%'
+            );
+        }
+    });
+}
+setInterval(doSomething,2000);
