69f6ccb0081d30617e2cfc9953d7bbfd4eb2fcf7
Darren Swan <mrdarren@turnsys.com>
2019-08-13 23:38:21 -0500

    Adding code

--- node_src/index.js
+++ node_src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+var sensor = require("node-dht-sensor");
+
+sensor.read(22, 4, function (err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + 'C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
+    }
+});
